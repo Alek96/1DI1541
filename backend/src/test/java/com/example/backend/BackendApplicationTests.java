2c2a70c37920addd6a2566d661098c068757927e
Aleksander Zamojski <zamojski96@gmail.com>
2022-10-16 17:02:33 +0200

    Set google style

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.backend;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
 
 }
