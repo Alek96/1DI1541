ba13664aa13424ce80e2a9a0944d48d3e6d61273
Aleksander Zamojski <zamojski96@gmail.com>
2022-11-29 23:03:44 +0100

    Add keycloak initializer

---
+++ keycloakInitializer/src/main/java/com/example/keycloakInitializer/KeycloakInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakInitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakInitializerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KeycloakInitializerApplication.class, args);
+  }
+
+}
